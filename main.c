fba6892fb4d0e064e5f1c021e0a8e04fe4f8c97a
Циклер Диана Витальевна <is541s28@lwp08.csc.local>
2016-05-30 10:53:14 +0600

    Korni

--- main.c
+++ main.c
@@ -6,4 +6,17 @@ int main() {
 	printf ("Input a, b, c:\n");
 	scanf ("%lf %lf %lf", &a, &b, &c);
 	d = b * b - 4 * a * c;
+	if (d < 0) {
+		printf ("Error!\n");
+		return 1;
+	}
+
+	if (d == 0) {
+		x1 = -b / (2 * a);
+		printf ("x = %.1lf\n", x1);
+		return 0;
+	}
+
+	x1 = (-b + sqrt(d)) / (2 * a);
+	x2 = (-b - sqrt(d)) / (2 * a);
 }
